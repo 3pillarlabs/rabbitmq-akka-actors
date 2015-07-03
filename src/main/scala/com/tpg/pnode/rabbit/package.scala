c5afe384e652be8835e909ae84effa02c72274ce
Cristian Robert <robert.cristian@gmail.com>
2015-07-03 10:32:01 +0300

    comments

---
+++ src/main/scala/com/tpg/pnode/rabbit/package.scala
@@ -0,0 +1,7 @@
+package com.tpg.pnode
+
+/**
+ * Provides classes for connecting to a RabbitMQ server and an actor that wraps
+ * a rabbit queue into a akka stream publisher
+ */
+package object rabbit {}

e852be53a3e1300fd240e26e5fc4143d6bbd1a0c
Cristian Robert <robert.cristian@gmail.com>
2015-07-31 11:08:16 +0300

    add config file

--- src/main/scala/com/tpg/rabbit/RabbitConn.scala
+++ src/main/scala/com/tpg/rabbit/RabbitConn.scala
@@ -4,6 +4,7 @@ import akka.actor.{ActorRef, ActorSystem}
 import com.github.sstone.amqp.Amqp.{AddQueue, QueueBind, QueueParameters}
 import com.github.sstone.amqp.{Amqp, ConnectionOwner, Consumer}
 import com.rabbitmq.client.ConnectionFactory
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -11,14 +12,18 @@ import scala.language.postfixOps
 /** functionality to connect to RabbitMQ */
 object RabbitConn {
 
+  val config = ConfigFactory.load()
+  val rabbitUri = config.getConfig("rabbit").getString("uri")
+
   /**
    * sets up an actor to handle a connection to a rabbit queue
    * @param system the system the actor will live in
    * @param handler a messages handler
    */
   def setUpRabbit(system: ActorSystem, handler: ActorRef) = {
+
     val connFactory = new ConnectionFactory()
-    connFactory.setUri("amqp://guest:guest@localhost/%2F")
+    connFactory.setUri(rabbitUri)
     val conn = system.actorOf(ConnectionOwner.props(connFactory, 1 second))
 
     val consumer = ConnectionOwner.createChildActor(conn, Consumer.props(handler, channelParams = None, autoack = false))
